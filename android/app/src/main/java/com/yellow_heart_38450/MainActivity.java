1b5c5c4321be1717d8210cfb17ab5135f2992276
Crowdbotics <team@crowdbotics.com>
2023-01-25 10:58:11 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/yellow_heart_38450/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yellow_heart_38450;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "yellow_heart_38450";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-heart-38450",
+  url: "https://yellow-heart-38450.botics.co",
+  api: "https://yellow-heart-38450.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
